f43a34f0c0795847bb2255f9329cd4678e0bc1a7
Tsarlove <olesyolshary@gmail.com>
2025-06-07 23:29:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,2 +1,70 @@
 // Copyright 2021 NNTU-CS
 
+#include <iostream>
+
+struct SYM {
+    char ch;
+    int prior;
+};
+
+template<typename T>
+struct Node {
+    T data;
+    Node* next;
+
+    Node(const T& d) : data(d), next(nullptr) {}
+};
+
+template<typename T>
+class TPQueue {
+private:
+    Node<T>* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (!isEmpty()) pop();
+    }
+
+    void push(const T& value) {
+        Node<T>* newNode = new Node<T>(value);
+
+        if (!head || value.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node<T>* current = head;
+            while (current->next && current->next->data.prior >= value.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    void pop() {
+        if (head) {
+            Node<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    const T& front() const {
+        if (!head) throw std::runtime_error("Queue is empty");
+        return head->data;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    void print() const {
+        Node<T>* current = head;
+        while (current) {
+            std::cout << current->data.ch << "(" << current->data.prior << ") ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
