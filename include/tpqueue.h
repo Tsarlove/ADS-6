8f796f7b95395d9fca832e7ade0fc8c5e9cd38fe
Tsarlove <olesyolshary@gmail.com>
2025-06-08 05:28:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,63 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+// Copyright 2025 NNTU-CS
+#ifndef TPQUEUE_H_
+#define TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#include <stdexcept>
 
 struct SYM {
   char ch;
   int prior;
+
+  SYM() : ch('\0'), prior(0) {}
+  explicit SYM(char c) : ch(c), prior(0) {}
+  SYM(char c, int p) : ch(c), prior(p) {}
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+template <typename T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    Node(const T& d) : data(d), next(nullptr) {}
+  };
+
+  Node* head = nullptr;
+
+ public:
+  TPQueue() = default;
+
+  void push(const T& val) {
+    Node* newNode = new Node(val);
+    if (!head || val.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next && val.prior <= current->next->data.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+
+  T pop() {
+    if (!head) throw std::out_of_range("Queue is empty");
+    Node* temp = head;
+    T result = head->data;
+    head = head->next;
+    delete temp;
+    return result;
+  }
+
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+};
+
+#endif  // TPQUEUE_H_
